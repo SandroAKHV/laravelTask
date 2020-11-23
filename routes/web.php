56d54151d4d6cc3890dffb7f21ea45d90d9bd3ba
MrDarkG <vako.batiashvili1998@gmail.com>
2020-11-23 20:46:01 +0400

    auth

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,11 @@ use Illuminate\Http\Request;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
